6b97614e70ce5cb15b70cc951de9d112d32d40c5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-12 16:04:27 +0100

    database, restricted middleware

--- database/users/user-router.js
+++ database/users/user-router.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
-const session = require('express-session')
 
 const Users = require('./user-model')
 
@@ -34,6 +33,7 @@ router.post('/login', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user;
           res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session')
 
 const UserRouter = require('./database/users/user-router');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 
 const server = express();
@@ -10,6 +11,26 @@ server.use(logger)
 server.use(express.json());
 server.use(session(sessionConfig));
 server.use('/api/users', UserRouter);
+
+
+const sessionConfig = {
+  name: 'test',
+  secret: 'keep this with you till death!',
+  cookie: {
+    maxAge: 1000 * 60 * 60, 
+    secure: false, // with secure, the cookie only gets set when https !!
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('./database/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}
 
 function logger(req, res, next) {
     console.log(`${req.method} to ${req.originalUrl}`)
