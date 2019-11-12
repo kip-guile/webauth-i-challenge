09c9782cdb6dc9da04dfcfb94559157070e58971
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-12 15:52:08 +0100

    day 2

--- database/users/user-router.js
+++ database/users/user-router.js
@@ -66,7 +66,7 @@ function restricted(req, res, next) {
       if (user && bcrypt.compareSync(password, user.password)) {
         next();
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'You shall not pass' });
       }
     })
     .catch(error => {
